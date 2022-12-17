632c26a6672d4c0d320e26b608973718715237b1
MyFreeR <my.free@mail.ru>
2022-12-17 12:52:07 +0300

    mountain

--- balls.py
+++ balls.py
@@ -1,6 +1,10 @@
 import os
-import random
 import pygame
+
+pygame.init()
+
+size = width, height = 800, 400
+screen = pygame.display.set_mode(size)
 
 
 def load_image(name, color_key=None):
@@ -20,56 +24,43 @@ def load_image(name, color_key=None):
     return image
 
 
+class Mountain(pygame.sprite.Sprite):
+    image = load_image("mountains.png")
 
-class Ball(pygame.sprite.Sprite):
-    def __init__(self, radius, x, y):
+    def __init__(self):
         super().__init__(all_sprites)
-        self.radius = radius
-        self.radius = radius
-        self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color("red"), (radius, radius), radius)
-        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = random.randint(-5, 5)
-        self.vy = random.randrange(-5, 5)
-
-    def update(self):  # проверка на столкновение с группами стенок:
-        self.rect = self.rect.move(self.vx, self.vy)
-        if pygame.sprite.spritecollideany(self, horizontal_borders):
-            self.vy = -self.vy
-        if pygame.sprite.spritecollideany(self, vertical_borders):
-            self.vx = -self.vx
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску сразу в конструкторе для эффективного сравнения
+        # чтобы потом сравнение на столкновение проходило быстрее.
+        self.mask = pygame.mask.from_surface(self.image)
+        # располагаем горы внизу
+        self.rect.bottom = height
 
 
-class Border(pygame.sprite.Sprite):
-    # строго вертикальный или строго горизонтальный отрезок
-    def __init__(self, x1, y1, x2, y2):
+# С переменной mountain будет работать класс-парашютик:
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
         super().__init__(all_sprites)
-        if x1 == x2:  # вертикальная стенка
-            self.add(vertical_borders)
-            self.image = pygame.Surface([1, y2 - y1])
-            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
-        else:  # горизонтальная стенка
-            self.add(horizontal_borders)
-            self.image = pygame.Surface([x2 - x1, 1])
-            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        # если еще в небе
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
 
 
 # группа, содержащая все спрайты
 all_sprites = pygame.sprite.Group()
-
-horizontal_borders = pygame.sprite.Group()
-vertical_borders = pygame.sprite.Group()
-
-size = width, height = 400, 300
-screen = pygame.display.set_mode(size)
-
-Border(5, 5, width - 5, 5)
-Border(5, height - 5, width - 5, height - 5)
-Border(5, 5, 5, height - 5)
-Border(width - 5, 5, width - 5, height - 5)
-
-for i in range(10):
-    Ball(20, 100, 100)
+# Создаем гору:
+mountain = Mountain()
 
 clock = pygame.time.Clock()
 
@@ -78,7 +69,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill(pygame.Color("white"))
+        # В игровом цикле добавляем парашютики:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Landing(event.pos)
+    screen.fill(pygame.Color("black"))
     all_sprites.draw(screen)
     all_sprites.update()
     pygame.display.flip()
